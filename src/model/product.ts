92dbb6fea45e14533db93412e296ad5ae56152f3
adrian <acalerobarrero@gmail.com>
2022-06-17 17:32:38 +0200

    Create a productModel

---
+++ src/model/product.ts
@@ -0,0 +1,19 @@
+import { iProduct } from "../interface/product";
+
+export class ProductModel implements iProduct {
+  id: number;
+  static generateId(): number {
+    return Math.ceil(Math.random() * 100_000);
+  }
+  constructor(
+    public name: string,
+    public brand: string,
+    public price: number,
+    public detail: string,
+    public stock: number,
+    public type: string,
+    public discount: boolean
+  ) {
+    this.id = ProductModel.generateId();
+  }
+}
