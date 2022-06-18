f3bf3641cbb6a1d848272b2bdb15f83b17005b56
adrian <acalerobarrero@gmail.com>
2022-06-18 19:01:32 +0200

    Add manual another product & interface store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { ProductModel } from "../model/product";
 import { productReducer } from "../redux/action.reducer";
+
+export interface iStore {
+  product: Array<ProductModel>;
+}
 
 export const preloadedState = {
   products: [],
