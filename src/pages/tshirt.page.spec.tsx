fb9a75e5d994269f3d00e1f786924523793e415a
adrian <acalerobarrero@gmail.com>
2022-06-19 01:14:41 +0200

    Added the provider in test of pages

--- src/pages/tshirt.page.spec.tsx
+++ src/pages/tshirt.page.spec.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 import TshirtPage from "./tshirt.page";
 
 describe("Given the component HoemPage", () => {
   describe("When i render the component", () => {
     test("Then i should be rendered", () => {
-      render(<TshirtPage />);
+      render(
+        <Provider store={store}>
+          <TshirtPage />
+        </Provider>
+      );
       expect(screen.getByText(/Camisetas/i)).toBeInTheDocument();
     });
   });
