a5a7f0ca4b7f96a75efd4c20fd821088cd52047e
adrian <acalerobarrero@gmail.com>
2022-06-19 01:14:08 +0200

    Adding form  in home page & her test

--- src/pages/home.page.spec.tsx
+++ src/pages/home.page.spec.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 import HomePage from "./home.page";
 
-describe("Given the component HoemPage", () => {
+describe("Given the component HomePage", () => {
   describe("When i render the component", () => {
     test("Then i should be rendered", () => {
-      render(<HomePage />);
+      render(
+        <Provider store={store}>
+          <HomePage />
+        </Provider>
+      );
       expect(screen.getByText(/Home/i)).toBeInTheDocument();
     });
   });
--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,30 +1,11 @@
+import { Form } from "../components/form/form";
 import "./home.page.css";
 
 export function HomePage() {
   return (
     <>
       <h2>Home</h2>
-      <form>
-        <select name="type" id="">
-          <option value="tipo">tipo</option>
-          <option value="zapatillas">zapatillas</option>
-          <option value="camisetas">camisetas</option>
-        </select>
-        <input type="text" placeholder="Nombre del Producto" />
-        <input type="text" placeholder="Marca del pructo" />
-        <input type="number" name="price" id="" placeholder="Precio" />
-        <textarea
-          name="detail"
-          id=""
-          cols={30}
-          rows={10}
-          placeholder="Detalles del producto..."
-        ></textarea>
-        <input type="number" name="stock" id="" placeholder="Stock" />
-        <label htmlFor="">¿Descuento?</label>
-        <input type="checkbox" name="" id="" />
-        <button type="submit">Crear</button>
-      </form>
+      <Form />
     </>
   );
 }
